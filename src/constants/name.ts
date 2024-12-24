ecfc9950a4ce1506dd36d96e181628449bf6d090
Joseph <166889171+0xj0s3ph@users.noreply.github.com>
2024-12-24 15:52:36 +0900

    feat: update names and format code

---
+++ src/constants/name.ts
@@ -0,0 +1,5 @@
+const firstName = "Joseph";
+const lastName = "Ju";
+const middleName = "Ryuzaki";
+export const fullName = `${firstName} "${middleName}" ${lastName}`;
+export const fullNameWithoutMiddle = `${firstName} ${lastName}`;
