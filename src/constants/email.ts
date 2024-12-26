ffa580b4a678b9d4e8d11fbaaf701f8defb65df7
Joseph <166889171+0xj0s3ph@users.noreply.github.com>
2024-12-26 17:51:07 +0900

    feat: improve contact me send email button

--- src/constants/email.ts
+++ src/constants/email.ts
@@ -1,3 +1,3 @@
-const emailUsername = "contact";
-const emailDomainname = "example.com";
+export const emailUsername = "contact";
+export const emailDomainname = "heyjoseph.me";
 export const email = emailUsername + "@" + emailDomainname;
