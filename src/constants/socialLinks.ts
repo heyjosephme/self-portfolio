a7e349f8d5a48867be1b6176b76ec39737825660
Joseph <166889171+0xj0s3ph@users.noreply.github.com>
2025-01-06 21:26:19 +0900

    feat: add linkedin link

--- src/constants/socialLinks.ts
+++ src/constants/socialLinks.ts
@@ -7,10 +7,17 @@ const metaSocialLinks = {
     domain: "github.com",
     username: "heyjosephme",
   },
+  linkedin: {
+    domain: "linkedin.com",
+    username: "heyjosephme",
+  },
 };
 
 export const urls = Object.fromEntries(
   Object.entries(metaSocialLinks).map(([key, value]) => {
+    if (key === "linkedin") {
+      return [key, `https://${value.domain}/in/${value.username}`];
+    }
     return [key, `https://${value.domain}/${value.username}`];
-  }),
+  })
 );
